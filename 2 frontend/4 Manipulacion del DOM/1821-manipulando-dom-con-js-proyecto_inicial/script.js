b72a0700d24f1dc0df61817f1dc5f43e35a357eb
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2023-06-23 12:53:44 -0500

    check list

--- 2 frontend/4 Manipulacion del DOM/1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 2 frontend/4 Manipulacion del DOM/1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,28 +1,49 @@
-const btn = document.querySelector("[data-from-btn]");
+(() => {
 
-const createTask = (evento) => {
+  const btn = document.querySelector("[data-from-btn]");
+
+  const createTask = (evento) => {
     evento.preventDefault();
+    //obtengo el input.
     const input = document.querySelector("[data-form-input]")
     const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
     input.value = "";
-    const content = 
-    `
-    <div>
-              <i class="far fa-check-square icon"></i>
-              <span class="task">${value}</span>
-            </div>
-            <i class="fas fa-trash-alt trashIcon icon"></i>
-    `
-    task.innerHTML = content;
+    //obtengo la lista ul
+    const list = document.querySelector("[data-list]");
 
+    //creo un item de la lista
+    const task = document.createElement("li");
+    task.classList.add("card"); //agrego el estilo al item
+
+    //crear contenedor
+    const taskContent = document.createElement("div");
+    //crear titulo
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+
+    //agregar icono check
+    taskContent.appendChild(checkComplete());
+    //agregar titulo
+    taskContent.appendChild(titleTask);
+    //agregar contendio a li, task
+    task.appendChild(taskContent);
+    //agregar li (task) a la lista ul
     list.appendChild(task);
+  }
 
-    console.log(content);
-}
+  btn.addEventListener("click", createTask)
 
-console.log(btn);
-
-btn.addEventListener("click", createTask)
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  }
+  const completeTask = (evento) => {
+    const element = evento.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  }
+})();
