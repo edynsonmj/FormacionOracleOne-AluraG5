98c4a3b9756e7ab424307f1b435b793c7749879f
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2023-10-30 16:26:08 -0500

    parte 1 style component

--- 2.1 React/styleComponents/style_component/src/Components/Title/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styled from "styled-components"
-
-const Title = ({children}) => {
-    return <h1 className="title">{children}</h1>
-};
-export default Title;
--- 3 React/styleComponents/style_component/src/App.js
+++ 3 React/styleComponents/style_component/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Header';
 import Container from './Components/Container';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle></GlobalStyle>
       <Header></Header>
       <Container></Container>
     </>
